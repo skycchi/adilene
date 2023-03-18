7946e1b113189c0b23c2854286593ca105a5109f
skycchi <128105252+skycchi@users.noreply.github.com>
2023-03-17 20:44:31 -0700

    Add files via upload

---
+++ webring/alternate/luka-widget.js
@@ -0,0 +1,77 @@
+// onionring.js is made up of four files - onionring-widget.js (this one!), onionring-index.js, onionring-variables.js and onionring.css
+// it's licensed under the cooperative non-violent license (CNPL) v4+ (https://thufie.lain.haus/NPL.html)
+// it was originally made by joey + mord of allium (蒜) house, last updated 2020-11-24
+
+// === ONIONRING-WIDGET ===
+//this file contains the code which builds the widget shown on each page in the ring. ctrl+f 'EDIT THIS' if you're looking to change the actual html of the widget
+
+var tag = document.getElementById("lukaring"); //find the widget on the page
+
+thisSite = window.location.href; //get the url of the site we're currently on
+thisIndex = null;
+
+// go through the site list to see if this site is on it and find its position
+for (i = 0; i < sites.length; i++) {
+  if (thisSite.startsWith(sites[i])) { //we use startswith so this will match any subdirectory, users can put the widget on multiple pages
+    thisIndex = i;
+    break; //when we've found the site, we don't need to search any more, so stop the loop
+  }
+}
+
+function randomSite() {
+  otherSites = sites.slice(); //create a copy of the sites list
+  otherSites.splice(thisIndex, 1); //remove the current site so we don't just land on it again
+  randomIndex = Math.floor(Math.random() * otherSites.length);
+  location.href = otherSites[randomIndex];
+}
+
+//if we didn't find the site in the list, the widget displays a warning instead
+if (thisIndex == null) {
+  tag.insertAdjacentHTML('afterbegin', `
+<table>
+  <tr>
+    <td>This site isn't part of the ${ringName} webring yet. Contact me at adilene@a.net to be added!</td>
+  </tr>
+</table>
+  `);
+}
+else {
+  //find the 'next' and 'previous' sites in the ring. this code looks complex
+  //because it's using a shorthand version of an if-else statement to make sure
+  //the first and last sites in the ring join together correctly
+  previousIndex = (thisIndex-1 < 0) ? sites.length-1 : thisIndex-1;
+  nextIndex = (thisIndex+1 >= sites.length) ? 0 : thisIndex+1;
+
+  indexText = ""
+  //if you've chosen to include an index, this builds the link to that
+  if (useIndex) {
+    indexText = `<a href='${indexPage}'>index</a>  `;
+  }
+
+  randomText = ""
+  //if you've chosen to include a random button, this builds the link that does that
+  if (useRandom) {
+    randomText = `<a  onclick='randomSite()' target='_parent'>random</a>  `;
+  }
+
+  //this is the code that displays the widget - EDIT THIS if you want to change the structure
+  tag.insertAdjacentHTML('afterbegin', `
+<base target="_parent">
+    <table class='lukacontainer' style='text-align: center;'>
+    <tr>
+        <td>
+            <div class='webring-info'>${ringName} WEBRING</div>
+            <div class='webring-links'>
+                <a target='_parent' href='${sites[previousIndex]}'><i class="fas fa-angle-left" style='margin-right:10px;'></i></a>
+                <a href='https://adilene.net/webring.html' target='_parent'>Index</a> · <a onclick='randomSite()' target='_parent'>Random</a> 
+                <a target='_parent' href='${sites[nextIndex]}'><i class="fas fa-angle-right" style='margin-left:10px;'></i></a>
+            </div>
+        </td>
+    </tr>
+    <tr>
+        <td style='text-align:center;'><img src='https://adilene.net/webring/images/luka.png'></td>
+    </tr>
+  </table>
+  `);
+
+}
---
+++ webring/webring-variables.js
@@ -0,0 +1,88 @@
+// onionring.js is made up of four files - onionring-widget.js, onionring-index.js, onionring-variables.js (this one!), and onionring.css
+// it's licensed under the cooperative non-violent license (CNPL) v4+ (https://thufie.lain.haus/NPL.html)
+// it was originally made by joey + mord of allium (蒜) house, last updated 2020-11-24
+
+// === ONIONRING-VARIABLES ===
+//this file contains the stuff you edit to set up your specific webring
+
+//the full URLs of all the sites in the ring
+var sites = [
+    "https://adilene.net/",
+    "https://lhfm.neocities.org/",
+    "https://pklucky.neocities.org/",
+    "https://argoxi.neocities.org/",
+    "https://burypink.neocities.org/",
+    "https://sugarangel.neocities.org/",
+    "https://snipertf2.neocities.org/",
+    "https://hellokittyminigun.neocities.org/",
+    "https://onlywonder.neocities.org/",
+    "https://boothworldindustries.neocities.org/",
+    "https://sleepy-sage.neocities.org/",
+    "https://psychonauts.neocities.org/",
+    "https://hunipyon.neocities.org/",
+    "https://charliedarling.neocities.org/",
+    "https://cozy-angels.neocities.org",
+    "https://shiroganes.neocities.org/",
+    "https://supernxva.neocities.org/",
+    "https://bodhisattva.neocities.org/",
+    "https://mystsaphyr.me/",
+    "https://kipperkorner.neocities.org/",
+    "https://moonlightgamingyt.neocities.org/",
+    "https://haruokumura.neocities.org/",
+    "https://90z-ash.neocities.org/",
+    "https://puddingpudds.neocities.org/",
+    "https://zoneoverimagination.neocities.org/",
+    "https://speedmaggots.neocities.org/",
+    "https://meko.neocities.org/",
+    "https://shadowfae.neocities.org/",
+    "https://blanketcape.neocities.org/",
+    "https://vocalopalooza.neocities.org/",
+    "https://korekiyovillage.neocities.org/",
+    "https://mirrorteru.neocities.org/",
+    "https://rayofhope.neocities.org/",
+    "https://ed1c24.neocities.org/",
+    "https://cyberealm.neocities.org/",
+    "https://rocktype.neocities.org/",
+    "https://jojjo.neocities.org/",
+    "https://xmenfan2001.neocities.org/",
+    "https://timverse.neocities.org/",
+    "https://superkirbylover.neocities.org/",
+    "https://expectationemesis.neocities.org/",
+    "https://amihan.neocities.org/",
+    "https://followthewhiterabbit.neocities.org/",
+    "https://sanguineroyal.neocities.org/",
+    "https://bp119.neocities.org/",
+    "https://thegvttr.neocities.org/",
+    "https://stupidbutcoolwebsitename.neocities.org/",
+    "https://25nightcord.neocities.org/",
+    "https://shroom.ink/",
+    "https://foreverliketh.is/",
+    "https://wishiwashi.neocities.org/",
+    "https://nonkiru.art/",
+    "https://layercake.neocities.org/",
+    "https://ruili.neocities.org/",
+    "https://hellstarares.neocities.org/",
+    "https://mikufan3939.neocities.org/",
+    "https://crescentmirror.neocities.org/",
+    "https://leanneu.neocities.org/",
+    "https://ophanimkei.com/",
+    "https://sunny-cities.neocities.org/",
+    "https://retrovampz.neocities.org/",
+    "https://pirupi.neocities.org/"
+];
+
+//the name of the ring
+var ringName = "VOCALOID";
+
+/* the unique ID of the widget. two things to note:
+ 1) make sure there are no spaces in it - use dashes or underscores if you must
+ 2) remember to change 'webringid' in the widget code you give out and all instances of '#webringid' in the css file to match this value!*/
+var ringID = "vocaring";
+
+//should the widget include a link to an index page?
+var useIndex = false;
+//the full URL of the index page. if you're not using one, you don't have to specify anything here
+var indexPage = "https://adilene.net";
+
+//should the widget include a random button?
+var useRandom = true;
