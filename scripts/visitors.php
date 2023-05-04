d1166d081d13d06bf478e00c418e79395016def4
Adilene Ricardo <adilene@Adilenes-MacBook-Air.local>
2023-05-04 05:26:09 -0700

    php

--- scripts/visitors.php
+++ scripts/visitors.php
@@ -1,54 +1,28 @@
 <?php
-/*************************************************************************
-php easy :: online visitors counter scripts set - PHP Include Version
-==========================================================================
-Author: php easy code, www.phpeasycode.com
-Web Site: http://www.phpeasycode.com
-Contact: webmaster @ phpeasycode.com
-*************************************************************************/
- 
-$dbfile = "visitors.db"; // path to data file
-$expire = 300; // average time in seconds to consider someone online before removing from the list
- 
-if(!file_exists($dbfile)) {
-die("Error: Data file " . $dbfile . " NOT FOUND!");
-}
- 
-if(!is_writable($dbfile)) {
-die("Error: Data file " . $dbfile . " is NOT writable! Please CHMOD it to 666!");
-}
- 
-function CountVisitors() {
-global $dbfile, $expire;
-$cur_ip = getIP();
-$cur_time = time();
-$dbary_new = array();
- 
-$dbary = unserialize(file_get_contents($dbfile));
-if(is_array($dbary)) {
-while(list($user_ip, $user_time) = each($dbary)) {
-if(($user_ip != $cur_ip) && (($user_time + $expire) > $cur_time)) {
-$dbary_new[$user_ip] = $user_time;
-}
-}
-}
-$dbary_new[$cur_ip] = $cur_time; // add record for current user
- 
-$fp = fopen($dbfile, "w");
-fputs($fp, serialize($dbary_new));
-fclose($fp);
- 
-$out = sprintf("%03d", count($dbary_new)); // format the result to display 3 digits with leading 0's
-return $out;
-}
- 
-function getIP() {
-if(isset($_SERVER['HTTP_X_FORWARDED_FOR'])) $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-elseif(isset($_SERVER['REMOTE_ADDR'])) $ip = $_SERVER['REMOTE_ADDR'];
-else $ip = "0";
-return $ip;
-}
- 
-$visitors_online = CountVisitors();
+
+$host = "localhost";
+$username = "username";
+$password = "password";
+$database = "database1";
+$table = "useronline";
+mysql_connect("$host", "$username", "$password") or die(mysql_error());
+mysql_select_db("$database") or die(mysql_error());
+session_start();
+$session = session_id();
+$time = time();
+$mysql = "SELECT * FROM $table WHERE session='$session'";
+$result = mysql_query($mysql);
+$count = mysql_num_rows($result);
+if($count == NULL)
+   mysql_query("INSERT INTO $table (session, time) VALUES ('$session', '$time')
+   else
+   mysql_query(UPDATE $table SET time='$time' WHERE session = '$session'");
+   $count_user = mysql_query("SELECT * FROM $table");
+   $count_user = mysql_num_rows($count_user);
+   echo"$count_user";
+
+
+
+
+
 ?>
-<p>Visitors online: <b><?=$visitors_online;?></b></p>
