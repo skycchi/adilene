13f75b1f253ade5fd85100d12872a6dcd2266ee2
Adilene Ricardo <adilene@Adilenes-MacBook-Air.local>
2023-05-04 05:36:44 -0700

    Update index.php

--- index.php
+++ index.php
@@ -114,7 +114,34 @@
                     <header>STATS</header>
                 </div>
                 <div class="inner" style="display:flex;flex-direction:column;justify-content:space-between;height:100%;text-align:center;color:var(--color);">
-                    <?php include("visitors.php");?>
+                    <?php
+
+$host = "localhost";
+$username = "username";
+$password = "password";
+$database = "database1";
+$table = "useronline";
+mysql_connect("$host", "$username", "$password") or die(mysql_error());
+mysql_select_db("$database") or die(mysql_error());
+session_start();
+$session = session_id();
+$time = time();
+$mysql = "SELECT * FROM $table WHERE session='$session'";
+$result = mysql_query($mysql);
+$count = mysql_num_rows($result);
+if($count == NULL)
+   mysql_query("INSERT INTO $table (session, time) VALUES ('$session', '$time')
+   else
+   mysql_query(UPDATE $table SET time='$time' WHERE session = '$session'");
+   $count_user = mysql_query("SELECT * FROM $table");
+   $count_user = mysql_num_rows($count_user);
+   echo"$count_user";
+
+
+
+
+
+?>
                     <div>Hits: <span id="hitcount"></span></div>
                     <div>Last Updated: <span id="updated"></span></div>
                 </div>
