782e6dd12e420f9fe035cc893da515d6dc650737
Adilene Ricardo <adilene@Adilenes-MacBook-Air.local>
2023-05-04 14:20:51 -0700

    dew ndjnj

--- pinger.php
+++ pinger.php
@@ -1,50 +1,69 @@
 <?php
+// Script Online Users and Visitors - https://coursesweb.net/php-mysql/
+if(!isset($_SESSION)) session_start(); // start Session, if not already started
 
-ini_set("display_errors", "1");
+$filetxt = 'userson.txt'; // the file in which the online users /visitors are stored
+$timeon = 120; // number of secconds to keep a user online
+$sep = '^^'; // characters used to separate the user name and date-time
+$vst_id = '-vst-'; // an identifier to know that it is a visitor, not logged user
 
-error_reporting(E_ALL);
-chmod 644 pinger.php;
+/*
+ If you have an user registration script,
+ replace $_SESSION['nume'] with the variable in which the user name is stored.
+ You can get a free registration script from: https://coursesweb.net/php-mysql/register-login-script-users-online_s2
+*/
 
-// Look for token
-$token = (isset($_GET['token']) && preg_match('/^[0-9a-f]{8}$/', $_GET['token'])) ? $_GET['token'] : false;
+// get the user name if it is logged, or the visitors IP (and add the identifier)
+$uvon = isset($_SESSION['nume']) ? $_SESSION['nume'] : $_SERVER['SERVER_ADDR']. $vst_id;
 
-if (!$token) {
-  $token = sprintf('%08x', crc32(microtime()));
+$rgxvst = '/^([0-9\.]*)'. $vst_id. '/i'; // regexp to recognize the line with visitors
+$nrvst = 0; // to store the number of visitors
+
+// sets the row with the current user /visitor that must be added in $filetxt (and current timestamp)
+$addrow[] = $uvon. $sep. time();
+
+// check if the file from $filetxt exists and is writable
+if(is_writable($filetxt)) {
+ // get into an array the lines added in $filetxt
+ $ar_rows = file($filetxt, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+ $nrrows = count($ar_rows); // number of rows
+
+ // if there is at least one line, parse the $ar_rows array
+ if($nrrows>0) {
+ for($i=0; $i<$nrrows; $i++) {
+ // get each line and separate the user /visitor and the timestamp
+ $ar_line = explode($sep, $ar_rows[$i]);
+
+ // add in $addrow array the records in last $timeon seconds
+ if($ar_line[0]!=$uvon && (intval($ar_line[1])+$timeon)>=time()) {
+ $addrow[] = $ar_rows[$i];
+ }
+ }
+ }
 }
 
-// get current minute, build APC key
-$quadrant = ceil(date_create()->format('s') / 15); // between 1-4
-$previousQuadrant = $quadrant - 1 < 1 ? 4 : $quadrant - 1;
-$key = 'pinger_'.$quadrant; 
-$previousKey = 'pinger_'.$previousQuadrant;
-
-// get tokens for the last 30 seconds
-$current = apcu_fetch($key);
-$previous = apcu_fetch($previousKey);
-
-if (!is_array($current)) {
-  $current = array();
+$nruvon = count($addrow); // total online
+$usron = ''; // to store the name of logged users
+// traverse $addrow to get the number of visitors and users
+for($i=0; $i<$nruvon; $i++) {
+ if(preg_match($rgxvst, $addrow[$i])) $nrvst++; // increment the visitors
+ else {
+ // gets and stores the user's name
+ $ar_usron = explode($sep, $addrow[$i]);
+ $usron .= '<br/> - <i>'. $ar_usron[0]. '</i>';
+ }
 }
+$nrusr = $nruvon - $nrvst; // gets the users (total - visitors)
 
-if (!is_array($previous)) {
-  $previous = array();
-}
+// the HTML code with data to be displayed
+$reout = '<div id="uvon"><h4>Online: '. $nruvon. '</h4>Visitors: '. $nrvst. '<br/>Users: '. $nrusr. $usron. '</div>';
 
-// Add current token if not found
-if (count($current) < 250 && !in_array($token, $current)) {
-  $current[] = $token;
-  apcu_store($key, $current, 31);
-}
+// write data in $filetxt
+if(!file_put_contents($filetxt, implode("\n", $addrow))) $reout = 'Error: Recording file not exists, or is not writable';
 
+// if access from <script>, with GET 'uvon=showon', adds the string to return into a JS statement
+// in this way the script can also be included in .html files
+if(isset($_GET['uvon']) && $_GET['uvon']=='showon') $reout = "document.write('$reout');";
 
-// Build return object: userCount, token
-$output = array(
-  'userCount' => count($current) > 250 ? '250+' : count(array_unique(array_merge($current, $previous))),
-  'token' => $token,
-);
-
-header('Content-Type: application/json');
-print json_encode($output);
-exit;
-
-
+echo $reout; // output /display the result
+?>
